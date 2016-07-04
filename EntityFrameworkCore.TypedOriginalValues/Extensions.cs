060973abdd48004cb5283cef1f0cca0f15a9d974
Nick Strupat <nick.strupat@gmail.com>
2016-07-04 14:07:00 -0400

    remove obsolete extension from EFCore build since the EFCore.TypedOV package never existed before

--- EntityFrameworkCore.TypedOriginalValues/Extensions.cs
+++ EntityFrameworkCore.TypedOriginalValues/Extensions.cs
@@ -13,10 +13,12 @@ namespace EntityFramework.TypedOriginalValues {
 #endif
 
 	public static class Extensions {
+#if !EF_CORE
 		[Obsolete("This extension is changing to `GetOriginal`. It will be removed in a future release.")]
 		public static TEntity GetOriginalValues<TEntity>(this DbContext context, TEntity entity) where TEntity : class {
 			return context.GetOriginal(entity);
 		}
+#endif
 
 		public static TEntity GetOriginal<TEntity>(this DbContext context, TEntity entity) where TEntity : class {
 			return OriginalValuesWrapper<TEntity>.Create(context.Entry(entity));
