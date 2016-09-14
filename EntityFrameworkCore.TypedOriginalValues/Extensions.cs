9b73b334941b09a0ecba7f323eba532655572b3b
Nick Strupat <nick.strupat@gmail.com>
2016-09-14 16:50:50 -0400

    update version numbers

--- EntityFrameworkCore.TypedOriginalValues/Extensions.cs
+++ EntityFrameworkCore.TypedOriginalValues/Extensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-#if EF_CORE
+﻿#if EF_CORE
 
 using Microsoft.EntityFrameworkCore;
 namespace EntityFrameworkCore.TypedOriginalValues {
@@ -13,13 +11,6 @@ namespace EntityFramework.TypedOriginalValues {
 #endif
 
 	public static class Extensions {
-#if !EF_CORE
-		[Obsolete("This extension is changing to `GetOriginal`. It will be removed in a future release.")]
-		public static TEntity GetOriginalValues<TEntity>(this DbContext context, TEntity entity) where TEntity : class {
-			return context.GetOriginal(entity);
-		}
-#endif
-
 		public static TEntity GetOriginal<TEntity>(this DbContext context, TEntity entity) where TEntity : class {
 			return OriginalValuesWrapper<TEntity>.Create(context.Entry(entity));
 		}
